18edf6d024f80ed6df7637c30397e00df27dfd82
chenxuanfeng <chenxuanfeng@meituan.com>
2020-08-09 18:20:14 +0800

    up

--- src/util/bookmark.ts
+++ src/util/bookmark.ts
@@ -3,15 +3,25 @@ const bookmarkList = [
     name: '学习',
     list: [
       { name: 'github', link: 'https://github.com/' },
-      { name: '掘金', link: 'https://juejin.im/' }],
+      { name: '掘金', link: 'https://juejin.im/' },
+    ],
   },
   {
     name: '工具',
     list: [
+      { name: 'MDN', link: 'https://developer.mozilla.org/zh-CN/docs/Web' },
       { name: '阿里巴巴矢量图标', link: 'https://www.iconfont.cn/' },
       { name: '图片压缩', link: 'https://tinypng.com/' },
       { name: '绘图', link: 'https://whimsical.com/' },
-      { name: 'json格式化', link: 'https://www.json.cn/' }],
+      { name: 'json格式化', link: 'https://www.json.cn/' },
+      { name: 'vvcloud VPN', link: 'https://vvcloud.me/' },
+    ],
+  },
+  {
+    name: '设计',
+    list: [
+      { name: 'isux', link: 'https://isux.tencent.com/' },
+    ],
   },
 ];
 
